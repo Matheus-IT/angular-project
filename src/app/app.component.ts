68fd89b2307acd76153dbff362b31b11a4e24a9f
matheuscosta <matheuscosta.tech@gmail.com>
2023-12-09 17:54:46 -0300

    Creating necessary components and setting up the menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
+    MatMenuModule,
+    FooterComponent,
+    LoginComponent,
+    MainPageComponent,
+    MenuComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-project';
+  title = 'LH Games';
 }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+  ],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
